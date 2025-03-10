0153c2df99eb9ed26c5e46ca11e6d371bffa338a
Miguel Angel <m.hornos@sapalomera.cat>
2025-03-10 16:23:47 +0100

    hotfix: corregit bug a index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<!-- correcció de bug -->
+
 <!DOCTYPE html>
 <!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
 <!--[if IE 7]> <html class="lt-ie9 lt-ie8" lang="en"> <![endif]-->
